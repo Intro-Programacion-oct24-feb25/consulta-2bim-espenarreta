5a404d7091f6ba8fe3c0660192c1e097e6303687
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2025-01-28 20:35:51 -0500

    Cambios

--- ejemplo_python/ejemplo.py
+++ ejemplo_python/ejemplo.py
@@ -2,7 +2,7 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        return n * factorial(n - 1)
+        return n * factorial(n - 1)  # recursividad
 
 numero = 5
 print(f"El factorial de {numero} es: {factorial(numero)}")
